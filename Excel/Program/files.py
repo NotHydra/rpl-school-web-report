2aacb6143cf87f1e5f8bcbec617a44b603831022
NotHydra <rizky.irswanda115@gmail.com>
2021-09-27 15:56:29 +0800

    File Update

--- Excel/Program/files.py
+++ Excel/Program/files.py
@@ -8,7 +8,8 @@ wb_mingguan_file = [
     "Excel/Mingguan/Excel/List Tugas Minggu Ke-6 (30 Agustus 2021 - 5 September 2021).xlsx",
     "Excel/Mingguan/Excel/List Tugas Minggu Ke-7 (6 September 2021 - 12 September 2021).xlsx",
     "Excel/Mingguan/Excel/List Tugas Minggu Ke-8 (13 September 2021 - 19 September 2021).xlsx",
-    "Excel/Mingguan/Excel/List Tugas Minggu Ke-9 (20 September 2021 - 26 September 2021).xlsx"
+    "Excel/Mingguan/Excel/List Tugas Minggu Ke-9 (20 September 2021 - 26 September 2021).xlsx",
+    "Excel/Mingguan/Excel/List Tugas Minggu Ke-10 (27 September 2021 - 3 Oktober 2021).xlsx"
 ]
 
 global wb_bulanan_file
@@ -27,7 +28,8 @@ wb_mingguan_image_name = [
     "Image List Tugas Minggu Ke-6",
     "Image List Tugas Minggu Ke-7",
     "Image List Tugas Minggu Ke-8",
-    "Image List Tugas Minggu Ke-9"
+    "Image List Tugas Minggu Ke-9",
+    "Image List Tugas Minggu Ke-10"
 ]
 
 global wb_bulanan_image_name
@@ -37,19 +39,19 @@ wb_bulanan_image_name = [
 ]
 
 global weekly_task_start_column_range, weekly_task_end_column_range
-weekly_task_start_column_range = [3, 8, 16, 23, 26, 34, 39, 42, 47]
-weekly_task_end_column_range   = [8, 16, 23, 26, 34, 39, 42, 47, 53]
+weekly_task_start_column_range = [3, 8,  16, 23, 26, 34, 39, 42, 47, 53]
+weekly_task_end_column_range   = [8, 16, 23, 26, 34, 39, 42, 47, 53, 54]
 
 global monthly_task_start_column_range,monthly_task_end_column_range
 monthly_task_start_column_range = [3, 34]
-monthly_task_end_column_range   = [34, 53]
+monthly_task_end_column_range   = [34, 54]
 
 global wb_bulanan_start_container, wb_bulanan_end_container # How much week it contains
 wb_bulanan_start_container = [1, 6]
-wb_bulanan_end_container   = [6, 10]
+wb_bulanan_end_container   = [6, 11]
 
 global wb_bulanan_subtract
 wb_bulanan_subtract = [0, 31]
 
 global combined_task_range
-combined_task_range = "A1:AZ42"
+combined_task_range = "A1:BA42"
