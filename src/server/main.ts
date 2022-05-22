650813a9f76c80730627010d06cac2c10f139de4
NotHydra <rizky.irswanda115@gmail.com>
2022-05-23 07:59:51 +0800

    feat: log start url

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(port)
+    .then(() => Logger.log(`Listening on http://localhost:${port}`));
 }
 bootstrap();
