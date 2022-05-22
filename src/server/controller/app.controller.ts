ff444cd395be929954247963a53c388ffa4395c2
NotHydra <rizky.irswanda115@gmail.com>
2022-05-22 13:07:41 +0800

    feat: fetch all student data by assignment week

--- src/server/controller/app.controller.ts
+++ src/server/controller/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from '../service/app.service';
 
 @Controller('/api')
@@ -8,5 +8,12 @@ export class AppController {
   @Get('/student/all/assignment/all/subject/all')
   getStudentAll() {
     return this.appService.getStudentAll();
+  }
+
+  @Get('/student/all/assignment/week/:count/subject/all')
+  getStudentAllByAssignmentWeek(
+    @Param('count', new ParseIntPipe()) count: number,
+  ) {
+    return this.appService.getStudentAllByAssignmentWeek(count);
   }
 }
--- src/server/service/app.service.ts
+++ src/server/service/app.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { map } from 'rxjs';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
@@ -10,6 +11,38 @@ export class AppService {
     let student_data = this.httpService
       .get('http://localhost:3000/api/student')
       .pipe(map((response) => response.data));
+
+    return student_data;
+  }
+
+  async getStudentAllByAssignmentWeek(count: number) {
+    const assignment_response = await axios.get(
+      `http://localhost:3000/api/assignment/week/${count}`,
+    );
+
+    let assignment_id = [];
+    assignment_response.data.forEach((dict) => {
+      assignment_id.push(dict.id);
+    });
+
+    const student_response = await axios.get(
+      'http://localhost:3000/api/student',
+    );
+
+    let student_response_array = [];
+    student_response.data.forEach((dict) => student_response_array.push(dict));
+
+    const student_data = student_response_array.filter((student) => {
+      const filtered_assignment = student.assignment.filter((assignment) => {
+        if (assignment_id.includes(assignment.id)) {
+          return assignment;
+        }
+      });
+
+      student.assignment = filtered_assignment;
+
+      return student;
+    });
 
     return student_data;
   }
