a7d6ccb5ca8290553586b2dd2ef5412f6c110593
NotHydra <rizky.irswanda115@gmail.com>
2022-07-12 23:21:23 +0800

    feat: utility convert status

--- scripts/python/utility.py
+++ scripts/python/utility.py
@@ -1,9 +1,17 @@
 import json
 
 class Utility():
+    switchStatus = {
+        None: 0,
+        "ü": 1,
+        "NON-MUS": 3
+    }
+
     def writeJSON(path, data):
         with open(path, "w") as outfile:
             outfile.write(json.dumps(data, indent=4))
 
     
+    def convertStatus(status):
+        return Utility.switchStatus.get(status)
     
