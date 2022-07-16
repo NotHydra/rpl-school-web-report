a659e73537e3386d1d0b8b6323088a5040c4aa49
NotHydra <rizky.irswanda115@gmail.com>
2022-07-17 00:03:15 +0800

    refactor: server directory

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
+import { AssignmentModule } from "./models/assignments/assignment.module";
 
 @Module({
-    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DEVELOPMENT_MONGODB_URI)],
+    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DEVELOPMENT_MONGODB_URI), AssignmentModule],
     controllers: [AppController],
     providers: [],
 })
