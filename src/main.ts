533da791a5128c60d2e49b0670b0325755d13bb8
NotHydra <rizky.irswanda115@gmail.com>
2022-07-16 23:45:43 +0800

    feat: listening on message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(process.env.PORT).then(() => {
+        Logger.log(`Listening on http://localhost:${process.env.PORT}`);
+    });
 }
 bootstrap();
