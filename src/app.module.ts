b489b53a2914f306a4a9f2082f63fe094fb17d74
NotHydra <rizky.irswanda115@gmail.com>
2022-07-16 23:54:45 +0800

    feat: mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DEVELOPMENT_MONGODB_URI)],
     controllers: [AppController],
     providers: [],
 })
